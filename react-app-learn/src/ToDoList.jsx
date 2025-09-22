0848a25087310c07adfd3d4c7e12df178b64c203
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2025-09-22 09:08:10 +0545

    todo list

--- react-app-learn/src/ToDoList.jsx
+++ react-app-learn/src/ToDoList.jsx
@@ -11,37 +11,51 @@ function ToDoList() {
 
   function handleInputChange(event) {
     setNewTask(event.target.value);
-
-
   }
 
   function addTask() {
-
+    if (newTask.trim() !== "") {
+      setTasks(t => [...tasks, newTask]);
+      setNewTask("");
+    }
   }
 
   function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
 
   }
 
   function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
 
   }
 
+
   function moveTaskDown(index) {
+    if (index < tasks.length - 1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+      setTasks(updatedTasks);
+    }
 
   }
 
   return (<div className="to-do-list">
-    <h1>To-Do-List</h1>
+    <h1 className="heading">To-Do-List</h1>
 
     <div>
       <input type="text" placeholder="Enter a task..." value={newTask}
-        onChange={handleInputChange} />
+        onChange={handleInputChange} className="input" />
       <button className="add-button" onClick={addTask} >Add A Task</button>
 
       <ol>
         {tasks.map((task, index) =>
-          <li key={index}>
+          <li key={index} className="list">
             <span className="text">{task}</span>
             <button className="delete-button"
               onClick={() => deleteTask(index)}>
