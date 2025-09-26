74162f57df43c84012330388902c544431acb8ae
neupanerabin7@gmail.com <neupanerabin7@gmail.com>
2025-09-27 00:08:01 +0545

    use effect

--- react-app-learn/src/UseEffect.jsx
+++ react-app-learn/src/UseEffect.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 function MyComponents() {
-  const [count, setCount] = useState(0);
+  /* const [count, setCount] = useState(0);
   const [color, setColor] = useState("green");
 
   function addCount() {
@@ -19,14 +19,43 @@ function MyComponents() {
   useEffect(() => {
     document.title = `Count: ${count} ${color}`;
 
-  }, [count]);
+  }, [count, color]);
+
+  */
+
+  const [width, setWeidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("EVENT LISTENER ADDED")
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("Event listener remove")
+    }
+  }, []);
+
+  useEffect(() => {
+    document.title = `Size: ${width} x ${height}`;
+  }, [width, height])
+
+
+
+  function handleResize() {
+    setWeidth(window.innerWidth);
+    setHeight(window.innerHeight)
+  }
 
 
   return (<>
-    <p style={{ color: color }}>Count: {count}</p>
+    {/* <p style={{ color: color }}>Count: {count}</p>
     <button onClick={addCount}>Add</button>
     <button onClick={subtractCount}>Subtract</button> <br />
-    <button onClick={changeColor}>Change Color </button>
+    <button onClick={changeColor}>Change Color </button>  */}
+
+    <p>Window Width: {width}px</p>
+    <p>Window Height: {height}px</p>
   </>);
 }
 
